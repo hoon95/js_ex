dd17ef2f5cc20dbd9fff041fa9fc939b3de4f40c
hoon95 <maruon95@gmail.com>
2024-10-21 14:18:00 +0900

    Update Figlet.js

---
+++ 04.nodejs/figlet.js
@@ -0,0 +1,29 @@
+var figlet = require('figlet');
+
+figlet("Hello World!!", (err, data) => {
+    if(err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return
+    }
+    console.log(data);
+});
+
+figlet.text(
+    "Boo!",
+    {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
