6c0f26f100da525ec44796464507e528aeee1c02
hoon95 <66771509+hoon95@users.noreply.github.com>
2024-10-29 19:08:36 +0900

    3번 풀이 - 시간 양식수정

--- 6.project/generator/order/OrderAtGenerator.js
+++ 6.project/generator/order/OrderAtGenerator.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
+const Datetime = require('../user/BirthdateGenerator');
 
 class OrderAtGenerator { 
     generateOrderAt() {
-        const today = new Date();
-        const year = today.getFullYear();
+        const date = new Datetime();
+        const dateYMD = date.generateBirthdate().fullYear;
+        
+        let hour = Math.floor(Math.random() * 24);
+        let minute = Math.floor(Math.random() * 60);
+        let second = Math.floor(Math.random() * 60);
+
+        hour = String(hour).padStart(2,'0');
+        minute = String(minute).padStart(2,'0');
+        second = String(second).padStart(2,'0');
+
+        const dateHMS = `${hour}:${minute}:${second}`;
+        
+        return `${dateYMD} ${dateHMS}`;
     }
 }
 
